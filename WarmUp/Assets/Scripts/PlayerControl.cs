3cd2783a3f06f0d4bb49e65032a4a7bb10c9f086
dannynanni <dannynanni.us@gmail.com>
2016-09-11 16:04:21 -0400

    Added WASD

---
+++ WarmUp/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float move;
+	Vector3 pos;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		pos = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if (Input.GetKey (KeyCode.W)) {
+			Debug.Log ("W");
+			rb.AddForce(transform.forward * move);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+
+			rb.AddForce (transform.forward * -move);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+
+			pos.x -= move;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+
+			pos.x += move;
+		}
+	}
+
+}
