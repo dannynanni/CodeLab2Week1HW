4118d57e53720f7110514e8f50433ae43a24bacf
dannynanni <dannynanni.us@gmail.com>
2016-09-14 10:40:15 -0400

    Added Physics Material + Sound

---
+++ WarmUp/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundScript : MonoBehaviour {
+
+	AudioSource bump;
+	// Use this for initialization
+	void Start () {
+	
+		bump = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.relativeVelocity.magnitude > 2)
+			bump.Play(); 
+	}
+}
